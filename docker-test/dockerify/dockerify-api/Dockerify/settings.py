5b67c56896b7e2131df50ed936ba9fab85a6f340
Yannick KIKI <seyive.kiki@gmail.com>
2021-05-24 20:21:07 +0100

    - stuffs

--- docker-test/dockerify/dockerify-api/Dockerify/settings.py
+++ docker-test/dockerify/dockerify-api/Dockerify/settings.py
@@ -77,12 +77,35 @@ WSGI_APPLICATION = 'Dockerify.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+from django.db.backends import mysql
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "dockerify",
+        "USER": "dockerify_api",
+        "PASSWORD": "cyneta-suspega-korel",
+        "HOST": "127.0.0.1",
+        "PORT": "5432"
+    },
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "dockerify",
+#         "USER": "dockerify_api",
+#         "PASSWORD": "cyneta-suspega-korel",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432"
+#     },
+# }
 
 
 # Password validation
