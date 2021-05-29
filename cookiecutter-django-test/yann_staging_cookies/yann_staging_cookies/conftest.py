03ef4f59d82810ceef076d0f9bbe48d16cfc49a5
Yannick KIKI <seyive.kiki@gmail.com>
2021-05-29 21:11:21 +0100

    - Cookiecutter test

---
+++ cookiecutter-django-test/yann_staging_cookies/yann_staging_cookies/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from yann_staging_cookies.users.models import User
+from yann_staging_cookies.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
