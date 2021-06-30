e9636e5c418ea4864872de33579ec5081419ec7d
Yannick KIKI <seyive.kiki@gmail.com>
2021-06-30 16:24:00 +0100

    - Rien compris

---
+++ integrations_test/todoist/draft.py
@@ -0,0 +1,7 @@
+# api = TodoistAPI(
+#     api_token="xxx"  # https://todoist.com/prefs/integrations
+# )
+# projects = api.get('projects')
+# sections = api.get('sections', params={'project_id': 2262341594})
+# tasks = api.get('tasks', params={'section_id': 49852392})
+# parent_tasks = api.get('tasks', params={'section_id': 49852392, 'filter': '!subtask'})
--- integrations_test/todoist/script.py
+++ integrations_test/todoist/script.py
@@ -1,9 +1,8 @@
-import json
-
 import requests
+from pprint import pprint
 
 
-class TodoistAPI:
+class TodoistBaseAPI:
 
     def __init__(self, api_token):
         self.api_token = api_token
@@ -17,13 +16,29 @@ class TodoistAPI:
         return response.json()
 
 
-api = TodoistAPI(
-    api_token="xxx"  # https://todoist.com/prefs/integrations
-)
-projects = api.get('projects')
-sections = api.get('sections', params={'project_id': 2262341594})
-tasks = api.get('tasks', params={'section_id': 49852392})
-parent_tasks = api.get('tasks', params={'section_id': 49852392, 'filter': '!subtask'})
+class TodoistAPI:
 
-with open('tasks.json', 'w') as f:
-    json.dump(parent_tasks, f, indent=4)
+    def __init__(self, api_token):
+        self.api = TodoistBaseAPI(api_token=api_token)
+
+    def _get_section_id(self, section_name):
+        sections = self.api.get('sections')
+        for section in sections:
+            if section['name'] == section_name:
+                return section['id']
+        raise AssertionError(f"La section {section_name} semble ne pas exister. "
+                             f"Si c'est une erreur , contactes le Yannoveli.")
+
+    def get_tasks(self, section_name):
+        section_id = self._get_section_id(section_name)
+        tasks = self.api.get('tasks', params={'section_id': section_id})
+        tasks.sort(key=lambda task: task.get('parent_id') or 0)
+        return tasks
+
+
+if __name__ == '__main__':
+    api = TodoistAPI(
+        api_token="xxx"  # https://todoist.com/prefs/integrations
+    )
+    tasks = api.get_tasks(section_name="Done")
+    pprint(tasks, indent=2)
